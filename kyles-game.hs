91108041b6de68ccc807cdab2a733ed5a0d96be3
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-06 08:42:37 +0100

    initial commit

---
+++ kyles-game.hs
@@ -0,0 +1,31 @@
+import Data.Char (digitToInt)
+import Data.List (foldl')
+import Control.Monad
+
+main :: IO ()
+main = do
+  t <- getLine
+  let n = read t :: Int
+  forM_ [1..n] $ \i -> do
+    num <- getLine
+    pins <- getLine
+    putStrLn $ if solve 0 pins then "WIN" else "LOSE"
+
+pinsToInt :: String -> Int
+pinsToInt ps = binToInt [if p == 'I' then '1' else '0' | p <- ps]
+  where binToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+
+solve :: Int -> String -> Bool
+solve n s = case moves s of 
+              []     -> odd n
+              (x:xs) -> foldr (&&) (solve (n + 1) x) [solve (n + 1) m | m <- xs]
+
+-- get all moves possible from a state
+moves :: String -> [String]
+moves m = mov (reverse m) [] []
+  where mov [] _ ms = ms
+        mov ('I':[]) ys ms = ('X':ys):ms
+        mov ('I':'I':xs) ys ms = mov ('I':xs) ('I':ys) $ map (reverse xs ++) [('X':'X':ys),('I':'X':ys)] ++ ms
+        mov ('I':x:xs) ys ms = mov xs (x:'I':ys) $ (reverse xs ++ (x:'X':ys)) : ms
+        mov (x:xs) ys ms = mov xs (x:ys) ms
+
