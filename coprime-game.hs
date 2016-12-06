02efd9411eb41d69b04df6ec39c4a8b4e5e02b86
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-06 13:42:56 +0100

    replace unneccessary map use with set

--- coprime-game.hs
+++ coprime-game.hs
@@ -1,5 +1,7 @@
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 (!) = (Map.!)
 
@@ -17,20 +19,20 @@ maxBPM :: (Int -> Int -> Bool) -> [Int] -> [Int] -> Map Int Int -> Int
 maxBPM _ [] _ _               = 0
 maxBPM func (x:xs) ys matches = i + maxBPM func xs ys m
                               where i = if hasMatch then 1 else 0
-                                    (hasMatch, m, _) = bpm func x ys [] Map.empty matches
+                                    (hasMatch, m, _) = bpm func x ys [] Set.empty matches
 
 bpm :: (Int -> Int -> Bool) -> Int -> [Int] -> [Int] 
-    -> Map Int Int -> Map Int Int-> (Bool, Map Int Int, Map Int Int)
+    -> Set Int -> Map Int Int-> (Bool, Map Int Int, Set Int)
 bpm _ _ [] _ seen matches         = (False, matches, seen) 
 bpm func x (y:ys) yz seen matches = 
-    if not $ coprime x y && Map.notMember y seen 
+    if not $ coprime x y && Set.notMember y seen 
       then if Map.notMember y matches
-             then (True, (Map.insert y x matches), (Map.insert y x seen))
+             then (True, (Map.insert y x matches), (Set.insert y seen))
              else if hasMatch
                   then (True, (Map.insert y x m), s)
-                  else bpm func x ys (y:yz) (Map.insert y x s) m
+                  else bpm func x ys (y:yz) (Set.insert y s) m
       else bpm func x ys (y:yz) seen matches
-    where (hasMatch, m, s) = bpm func (matches ! y) ((y:ys)++yz) [] (Map.insert y x seen) matches
+    where (hasMatch, m, s) = bpm func (matches ! y) ((y:ys)++yz) [] (Set.insert y seen) matches
 
 coprime :: Int -> Int -> Bool
 coprime a b = (==1) $ gcd a b
