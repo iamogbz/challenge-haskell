5b7a74eacf0d355a2e1e9ea7f9044b692caa9afb
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-06 02:08:03 +0100

    attempt one

--- coprime-game.hs
+++ coprime-game.hs
@@ -1,4 +1,7 @@
-import Data.List (group, sort, sortBy)
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+(!) = (Map.!)
 
 main :: IO ()
 main = do
@@ -8,15 +11,26 @@ main = do
     let n1 = map (read::String->Int) $ words temp
     temp <- getLine
     let n2 = map (read::String->Int) $ words temp
-    let graph = makeBPGraph n1 n2;
-    putStrLn . show $ calcBPMax graph
+    putStrLn . show $ maxBPM coprime n1 n2 Map.empty
 
-calcBPMax :: [[Bool]] -> Int
-calcBPMax _ = 0
+maxBPM :: (Int -> Int -> Bool) -> [Int] -> [Int] -> Map Int Int -> Int
+maxBPM _ [] _ _               = 0
+maxBPM func (x:xs) ys matches = i + maxBPM func xs ys m
+                              where i = if hasMatch then 1 else 0
+                                    (hasMatch, m, _) = bpm func x ys [] Map.empty matches
 
-makeBPGraph :: [Int] -> [Int] -> [[Bool]]
-makeBPGraph xs ys = [mapBP x ys | x <- xs]
-                  where mapBP f = map $ not . coprime f
+bpm :: (Int -> Int -> Bool) -> Int -> [Int] -> [Int] 
+    -> Map Int Int -> Map Int Int-> (Bool, Map Int Int, Map Int Int)
+bpm _ _ [] _ seen matches         = (False, matches, seen) 
+bpm func x (y:ys) yz seen matches = 
+    if not $ coprime x y && Map.notMember y seen 
+      then if Map.notMember y matches
+             then (True, (Map.insert y x matches), (Map.insert y x seen))
+             else if hasMatch
+                  then (True, (Map.insert y x m), s)
+                  else bpm func x ys (y:yz) (Map.insert y x s) m
+      else bpm func x ys (y:yz) seen matches
+    where (hasMatch, m, s) = bpm func (matches ! y) ((y:ys)++yz) [] (Map.insert y x seen) matches
 
 coprime :: Int -> Int -> Bool
 coprime a b = (==1) $ gcd a b
