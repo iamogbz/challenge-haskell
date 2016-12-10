94b258037d477d21d084d227344a745bad6fcde9
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-10 08:48:17 +0100

    use game theory, grundy's nimbers and memoisation for solution

--- kyles-game.hs
+++ kyles-game.hs
@@ -1,6 +1,6 @@
-import Data.Char (digitToInt)
-import Data.List (foldl')
 import Control.Monad
+import Data.Bits
+import Data.List
 
 main :: IO ()
 main = do
@@ -9,24 +9,20 @@ main = do
   forM_ [1..n] $ \i -> do
     num <- getLine
     pins <- getLine
-    putStrLn $ if solve 0 pins then "WIN" else "LOSE"
+    putStrLn $ if solve pins then "WIN" else "LOSE"
 
-pinsToInt :: String -> Int
-pinsToInt ps = binToInt [if p == 'I' then '1' else '0' | p <- ps]
-  where binToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+solve :: String -> Bool
+solve = (/=0) . foldr xor 0 . map (nimber . length) . filter ((=='I') . head) . group
 
-solve :: Int -> String -> Bool
-solve n s = case moves s of 
-              []     -> odd n
-              (x:xs) -> foldr control (solve (n + 1) x) [solve (n + 1) m | m <- xs]
-            where control = if odd n then (&&) else (||)
+-- minimum excluded numbers
+mex :: [Int] -> Int
+mex xs = (\\) [0..] xs !!0
 
--- get all moves possible from a state
-moves :: String -> [String]
-moves m = mov (reverse m) [] []
-  where mov [] _ ms = ms
-        mov ('I':[]) ys ms = ('X':ys):ms
-        mov ('I':'I':xs) ys ms = mov ('I':xs) ('I':ys) $ map (reverse xs ++) [('X':'X':ys),('I':'X':ys)] ++ ms
-        mov ('I':x:xs) ys ms = mov xs (x:'I':ys) $ (reverse xs ++ (x:'X':ys)) : ms
-        mov (x:xs) ys ms = mov xs (x:ys) ms
-
+-- grundy numbers
+nimber :: Int -> Int
+nimber = (map g [0..] !!)
+  where
+    g 0 = 0
+    g n = mex $
+        [ nimber i `xor` nimber (n-1-i) | i <- [0..n-1] ] ++
+        [ nimber i `xor` nimber (n-2-i) | i <- [0..n-2] ]
