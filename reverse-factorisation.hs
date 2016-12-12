1ea929909a1974ebc72266bc758a088210c29bc8
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-12 03:06:42 +0100

    support for incomplete edge cases previously noted

--- reverse-factorisation.hs
+++ reverse-factorisation.hs
@@ -1,11 +1,5 @@
 import Data.List
-
--- INCOMPLETE SOLUTION!!!
--- doesn't work for some test cases e.g.
--- 36 [18,9,4] - this should give 1 4 36 [4,9] but it fails
--- 24 [4,6,8] etc.
--- probably do two passes, first from largest to smallest 
--- and then in reverse for elements not used
+import Data.Maybe
 
 main :: IO ()
 main = do
@@ -13,10 +7,19 @@ main = do
     let (n:_) = map (read::String->Int) $ words t
     t <- getLine
     let fs = map (read::String->Int) $ words t
-    let res = mul . reverse $ factors n fs
-    putStrLn $ if n == (head . reverse) res then concat . map (\x -> (show x)++" ") $ res else "-1"
+        s = mul . reverse . fromMaybe [] $ solve n fs
+    putStrLn $ if s == [0] then "-1" else concat . map (\x -> (show x)++" ") $ s
+    
+solve :: Int -> [Int] -> Maybe [Int]
+solve n []     = Nothing
+solve n fs = case n == (last . sort . mul) s of
+  True -> Just s
+  _    -> solve n ns
+  where s = factors n (f:ns) 
+        (f:ns) = reverse $ sort fs
     
 mul :: [Int] -> [Int]
+mul [] = [0]
 mul fs = mul' 1 fs
   where mul' n [] = [n]
         mul' n (f:fs) = n : (mul' (n*f) fs)
@@ -26,4 +29,4 @@ factors n ns =
   case factors' of
     [] -> []
     _  -> factors' ++ factors (n `div` (head factors')) ns
-  where factors' = take 1 $ filter (\x -> (n `mod` x) == 0) $ (reverse . sort) ns
+  where factors' = take 1 $ filter (\x -> (n `mod` x) == 0) ns
