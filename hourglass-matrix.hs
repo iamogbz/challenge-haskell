591efc6cfd0cf61d1fd5468fefe5ce0841c40e9d
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-12 04:01:47 +0100

    filter out incomplete hourglasses

--- hourglass-matrix.hs
+++ hourglass-matrix.hs
@@ -4,8 +4,8 @@ main :: IO ()
 main = do
   temp <- getLines 6
   let array = concat $ map (map (read :: String -> Int) . words) temp
-  putStrLn . show . maximum $ extract array
-    
+  putStrLn . show . maximum $ solve array
+
 getLines :: Int -> IO [String]
 getLines 0 = return []
 getLines n = do          
@@ -13,9 +13,10 @@ getLines n = do
         xs <- getLines (n-1)    
         return (x:xs)
 
-extract :: [Int] -> [Int]
-extract = map (sum . map snd) . groupBy match' . sort . concat . map build' . zip [0..]
-  where build' (i, n) = filter (\x -> fst x >= 0) [(i-d, n) | d <- [0,1,2,7,12,13,14]]
-        match' a b = fst a == fst b
+solve :: [Int] -> [Int]
+solve = map (sum . map snd) . groupBy match' . sort . concat . map build' . zip [0..]
+  where build' (i, n) = filter valid' [(i - d, n) | d <- [0,1,2,7,12,13,14]]
+        valid' (i, _) = i >= 0 && i <= 21 && mod i 6 < 4
+        match' a b    = fst a == fst b
 
 
