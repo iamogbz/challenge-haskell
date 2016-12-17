c21e5c777cb421ba25004ca2edd6444c5a94f516
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-17 08:09:56 +0100

    misere num

---
+++ misere-nim.hs
@@ -0,0 +1,16 @@
+import Control.Monad
+import Data.Bits
+import Data.List
+
+main :: IO ()
+main = do
+  t <- getLine
+  let n = read t :: Int
+  forM_ [1..n] $ \i -> do
+    num <- getLine
+    piles <- getLine
+    putStrLn $ if solve . map read $ words piles then "First" else "Second"
+
+solve :: [Int] -> Bool
+solve ns = (/=0) $ foldr xor s ns
+  where s = if all (==1) ns then 1 else 0 :: Int
