06a12a574bd346f1a1d52765cd260149e05a6e72
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-19 03:08:17 +0100

    decibinary first attempt

---
+++ decibinary.hs
@@ -0,0 +1,24 @@
+import Numeric
+import Data.Char
+import Data.List
+import qualified Data.Set as Set
+
+main :: IO ()
+main = interact $ unlines . map show . tail . lines
+        
+deciBin :: Int -> Int
+deciBin = (deciBins [0] !!)
+  where deciBins [] = []
+        deciBins xs = xs++(deciBins . unique . filter (\x -> x `mod` 10 /= 9) . concat $ [[x+1, x+9, toBin $ 1 + fromBin x] | x <- xs])
+
+unique :: Ord a => [a] -> [a]
+unique = map head . group . sort
+
+toBin :: Int -> Int
+toBin 0 = 0
+toBin n = read . concat . map show $ reverse (helper n)
+  where helper 0 = []
+        helper n = let (q,r) = n `divMod` 2 in r : helper q
+
+fromBin :: Int -> Int
+fromBin = foldl' (\acc x -> acc * 2 + digitToInt x) 0 . show
