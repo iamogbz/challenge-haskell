da73abaff4d4573c53ff98a5477574d826fe6d40
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-02-21 16:58:23 +0100

    custom data structure improved performance by ~> 67%

--- ctci-running-median.hs
+++ ctci-running-median.hs
@@ -4,19 +4,26 @@ import Data.List
 -- main :: IO ()
 main = interact $ unlines . map show . runningMedian . map read . drop 1 . lines
 
--- cascading median
 -- runningMedian :: [Int] -> [Double]
-runningMedian = rmed 0 []
-    where -- rmed :: Int -> [Int] -> [Int] -> [Double]
-          rmed _ _ [] = []
-          rmed l s (x:xs) = let sx = insert x s
-                                ln = l + 1
-                            in (median ln sx):rmed ln sx xs
+runningMedian = rmed emptyBList
+    where -- rmed :: BList -> [Int] -> [Double]
+          rmed _ [] = []
+          rmed b (x:xs) = let bs = binsert x b in (median bs):rmed bs xs
 
--- median :: Size, List -> Double
-median l xs = toDP 1 $ if even l 
-                then (/2) . realToFrac . sum . take 2 $ drop (n-1) xs 
-                else realToFrac $ xs !! n
-    where n = div l 2 -- middle of list
+-- median :: BList -> Double
+median (left,r:ight,s1,s2) = toDP 1 $ if s1 < s2 then r else (r + head left)/2
+    where
           -- round to specific number of decimal places
           toDP n f = (fromInteger $ round $ f * (10^n)) / (10.0^^n)
+
+-- custom data structure
+type BList = ([Int],[Int],Int,Int)
+emptyBList = ([],[],0,0)
+binsert x ([],[],_,_) = ([],[x],0,1)
+binsert x ([],r:ight,_,s2) = if r > x then ([x],r:ight,1,s2)
+                             else ([r], insert x ight,1,s2)
+binsert x (l:eft,r:ight,s1,s2) | s1 < s2 = if l < x then (r:l:eft,insert x ight,s1+1,s2)
+                                           else (l:insert' x eft,r:ight,s1+1,s2)
+                               | otherwise = if r > x then (insert' x eft,l:r:ight,s1,s2+1)
+                                             else (l:eft,r:insert x ight,s1,s2+1)
+                               where insert' x = insertBy (\a b -> compare b a) x
