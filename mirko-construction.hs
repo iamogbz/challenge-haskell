43aa0051b4c96e3231b26dce05b2900eaa54fcbc
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-04-05 20:13:07 -0400

    final mirko construction solution

--- mirko-construction.hs
+++ mirko-construction.hs
@@ -1,41 +1,46 @@
 import Data.List
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
 import Control.Monad
 
 main = do
-    ns <- getLine
-    let s:q:_ = map (read::String->Int) $ words ns
-    ns <- getLine
-    let hs = map (read::String->Int) $ words ns
-    ns <- getLine
-    let gs = map (read::String->Int) $ words ns
-    let xs = simulate $ prep hs gs
-    forM_ [1..q] $ \_ -> do
-        n <- getLine
-        print $ (xs!!) $ (read::String->Int) n
-    
--- triple where values are (height, growth, index)
-type TRPL = (Int, Int, Int)
+    (s:q:_) <- map (read::String->Int) . words <$> getLine
+    hs <- map (read::String->Int) . words <$> getLine
+    gs <- map (read::String->Int) . words <$> getLine
+    qs <- map (read::String->Int) . lines <$> getContents
+    let queried = simulate (unique qs) (prep hs gs) IM.empty
+    putStr . unlines $ map (show . (queried IM.!)) qs
 
-prep :: [Int] -> [Int] -> [TRPL]
-prep a b = sortBy order $ zip3 a b [1..]
+-- triple where values are (growth, height, index)
+data Building = B {g::Int, h::Int, k::Int}
 
-simulate :: [TRPL] -> [Int]
-simulate [] = []
-simulate xs@((_, _, i):_) = i:simulate nxs
+cmp :: Building -> Building -> Ordering
+cmp (B m a i) (B n b j) = compare (n, b, j) (m, a, i)
+
+check :: [Building] -> [Building]
+check (p1:ps1@(p2:ps2))
+            | g p1 == g p2 = check ps1
+            | otherwise =  p1 : check' (check ps1) where
+                check' pp@(p2:ps2@(p3:ps3))
+                    | (h p1 - h p2) * (g p3 - g p2) > (h p2 - h p3) * (g p2 - g p1) = check' ps2
+                    | otherwise = pp
+                check' ps = ps
+check ps = ps
+
+prep :: [Int] -> [Int] -> [Building]
+prep hs gs = sortBy cmp $ zipWith3 B gs hs [1..]
+
+-- remove duplicates, order is not preserved
+unique :: Ord a => [a] -> [a]
+unique = map head . group . sort
+
+simulate :: [Int] -> [Building] -> IntMap Int -> IntMap Int
+simulate [] _ rs = rs
+simulate (q:qs) p@[B _ _ i] rs = simulate qs p (IM.insert q i rs) 
+simulate qq@(q:qs) pp@(B m a i:ps@(B n b j:_)) rs
+    | aq < bq = simulate qq ps rs
+    | otherwise = simulate qs (last pp1:pp2) (IM.insert q (maximum $ map k pp1) rs)
     where
-        nxs = prune $ next xs
-        next xs = [(a + m, m, i) | (a, m, i) <- xs]
-
-prune :: [TRPL] -> [TRPL]
-prune = prune' [] . reverse
-    where
-        prune' s [] = s
-        prune' s [x] = x:s
-        prune' s (x@(a, _, i):y@(b, _, j):zs) = if a > b || a == b && i > j
-                                                then prune' s (x:zs)
-                                                else prune' (x:s) (y:zs)
-
-order :: TRPL -> TRPL -> Ordering
-order (a, _, i) (b, _, j) = if a == b
-                            then compare j i
-                            else compare b a
+        aq = a + m * q
+        bq = b + n * q
+        (pp1, pp2) = span (\(B o c _) -> aq == c + o * q) pp
